9880a0508d8fa3e138678efd5a543f7f406c03b1
Matheus <matheus.garibaldi8@gmail.com>
2023-10-17 16:29:42 -0300

    Exercícios realizados até ex7

--- ListaExProva1/Lista2/lista2.js
+++ ListaExProva1/Lista2/lista2.js
@@ -6,7 +6,7 @@ function ex1(...vetor) {
     console.log(vetor2);
 }
 // ex1(1, 2, 3, 4, 5, 6);
-function ex2_ex3() {
+function ex2_3_4() {
     let vetor1 = leitor.question("Escreva um numero de 5 algarismos: ").split("");
     let vetor2 = leitor.question("Escreva um numero de 5 algarismos: ").split("");
     function transformaVetor(vetor) {
@@ -17,22 +17,15 @@ function ex2_ex3() {
     }
     function somaVetor(vetorA, vetorB) {
         let vetorResultante = [];
-        // if (vetorA.length !== vetorB.length) {
-        //   return console.log(
-        //     "ambas as listas precisam ter o mesmo numero de indices"
-        //   );
-        // } else {
-        for (let index = 0; index < vetorA.length; index++) {
-            vetorResultante.push(vetorA[index] + vetorB[index]);
-            if (vetorA.length > vetorB.length) {
-                vetorResultante.push(vetorA[index]);
-            }
-            else if (vetorA.length < vetorB.length) {
-                vetorResultante.push(vetorB[index]);
-            }
+        if (vetorA.length !== vetorB.length) {
+            return console.log("ambas as listas precisam ter o mesmo numero de indices");
         }
-        return vetorResultante;
-        // }
+        else {
+            for (let index = 0; index < vetorA.length; index++) {
+                vetorResultante.push(vetorA[index] + vetorB[index]);
+            }
+            return vetorResultante;
+        }
     }
     vetor1 = transformaVetor(vetor1);
     vetor2 = transformaVetor(vetor2);
@@ -42,6 +35,76 @@ function ex2_ex3() {
     // ou poderia atribuir o o resultado referente ao argumento como "vetor1" a uma variável, neste caso, só atualizei o valor dos vetores
     // para não precisar criar mais duas variáveis. //
 }
-ex2_ex3();
-function ex4() {
+// ex2();
+function ex5() {
+    let entradaN = leitor.question("Digite 20 numeros separados por espaco: ");
+    let vetorN = entradaN.split(" ");
+    let novoVetor = [];
+    let tamanhoVetorN = vetorN.length;
+    for (let index = 0; index < tamanhoVetorN; index++) {
+        if (index < tamanhoVetorN / 2) {
+            novoVetor[index] = vetorN[tamanhoVetorN - 1 - index];
+        }
+        else {
+            novoVetor[index] = vetorN[index];
+        }
+    }
+    console.log(novoVetor);
 }
+// ex5();
+function ex6() {
+    let gabarito = [];
+    let tentativa;
+    let listaTentativa = [];
+    function criaGabarito() {
+        for (let index = 0; index < 14; index++) {
+            let numeroAleatorio;
+            tentativa = parseFloat(leitor.question(`Digite um numero entre 0 - 30: `));
+            listaTentativa.push(tentativa);
+            do {
+                numeroAleatorio = Math.ceil(Math.random() * 30);
+            } while (gabarito.includes(numeroAleatorio));
+            // se o número aleatório já estiver na lista de gabarito, é necessário gerar um novo número.
+            // então ele precisará de uma verificação varrendo todos os números já atribuídos na lista.
+            gabarito[index] = numeroAleatorio;
+        }
+        //comparando lista com tentativa//
+        let listaOdernada = listaTentativa.sort();
+        let gabaritoOrdenado = gabarito.sort();
+        let contador = 0;
+        for (let index = 0; index < gabaritoOrdenado.length; index++) {
+            if (gabaritoOrdenado[index] == listaOdernada[index]) {
+                contador++;
+            }
+        }
+        if (contador == 13) {
+            console.log("GANHADOR, PARABÉNS!");
+        }
+        else {
+            console.log(`Voce acertou ${contador} numeros. `);
+        }
+        return gabarito;
+    }
+    console.log(criaGabarito());
+}
+// ex6();
+function ex7() {
+    // coluna vira linha => matriz transposta.
+    // primeiro vou tentar montar uma matriz //
+    let colunas = parseFloat(leitor.question("Qtd colunas: "));
+    let linhas = parseFloat(leitor.question("Qtd linhas: "));
+    let matriz = [];
+    for (let i = 1; i < linhas + 1; i++) {
+        let linha = [];
+        for (let y = 1; y < colunas + 1; y++) {
+            let valor = parseFloat(leitor.question(`Valor para a posicao (linha ${i}, coluna ${y}: `));
+            linha.push(valor);
+        }
+        matriz.push(linha);
+    }
+    console.log("Matriz Resultante");
+    for (let i = 0; i < matriz.length; i++) {
+        console.log(matriz[i].join(" "));
+    }
+}
+ex7();
--- ListaExProva1/Lista2/lista2.ts
+++ ListaExProva1/Lista2/lista2.ts
@@ -1,3 +1,4 @@
+import { equal } from "assert";
 import { parseArgs } from "util";
 
 var leitor = require("readline-sync");
@@ -9,7 +10,7 @@ function ex1(...vetor: number[]) {
 
 // ex1(1, 2, 3, 4, 5, 6);
 
-function ex2_3() {
+function ex2_3_4() {
   let vetor1 = leitor.question("Escreva um numero de 5 algarismos: ").split("");
   let vetor2 = leitor.question("Escreva um numero de 5 algarismos: ").split("");
 
@@ -47,6 +48,102 @@ function ex2_3() {
 
 // ex2();
 
-function ex3(){
+function ex5() {
+  let entradaN = leitor.question("Digite 20 numeros separados por espaco: ");
+
+  let vetorN = entradaN.split(" ");
+
+  let novoVetor = [];
+
+  let tamanhoVetorN = vetorN.length;
+
+  for (let index = 0; index < tamanhoVetorN; index++) {
+    if (index < tamanhoVetorN / 2) {
+      novoVetor[index] = vetorN[tamanhoVetorN - 1 - index];
+    } else {
+      novoVetor[index] = vetorN[index];
+    }
+  }
+  console.log(novoVetor);
+}
+// ex5();
+
+function ex6() {
+  let gabarito: number[] = [];
+  let tentativa:number;
+  let listaTentativa:number[] = [];
+
+  function criaGabarito() {
+    for (let index = 0; index < 14; index++) {
+      let numeroAleatorio;
+
+      tentativa = parseFloat(leitor.question(`Digite um numero entre 0 - 30: `));
+
+      listaTentativa.push(tentativa);
+
+      do {
+        numeroAleatorio =  Math.ceil(Math.random() * 30)  
+      } while (gabarito.includes(numeroAleatorio));
+      // se o número aleatório já estiver na lista de gabarito, é necessário gerar um novo número.
+      // então ele precisará de uma verificação varrendo todos os números já atribuídos na lista.
+
+      gabarito[index] = numeroAleatorio;
+    }
+
+    //comparando lista com tentativa//
+
+    let listaOdernada = listaTentativa.sort();
+    let gabaritoOrdenado = gabarito.sort();
+    let contador = 0
+
+    for (let index = 0; index < gabaritoOrdenado.length; index++) {
+      if (gabaritoOrdenado[index] == listaOdernada[index]){
+        contador ++
+      }      
+    }
+    if (contador == 13){
+      console.log("GANHADOR, PARABÉNS!");
+    }
+    else{
+      console.log(`Voce acertou ${contador} numeros. `);
+    }
+    return gabarito;
+  }
+
+  console.log(criaGabarito());
+  }
+// ex6();
+
+function ex7(){
+  // coluna vira linha => matriz transposta.
+
+  // primeiro vou tentar montar uma matriz //
+  let colunas = parseFloat(leitor.question("Qtd colunas: "));
+  let linhas = parseFloat(leitor.question("Qtd linhas: "));
+  let matriz:number[][] = [];
+
+  for (let i = 1; i < linhas + 1; i ++){
+    
+    let linha:number[] = []
+
+    for (let y = 1; y < colunas + 1; y ++){
+
+      let valor = parseFloat(leitor.question(`Valor para a posicao (linha ${i}, coluna ${y}: `));
+      linha.push(valor);
+
+    }
+    matriz.push(linha);
+
+  }
+
+
+  console.log("Matriz Resultante");
+  for(let i = 0; i < matriz.length; i++){
+    console.log(matriz[i].join(" "));
+  }
+
 
 }
+
+
+ex7();   
